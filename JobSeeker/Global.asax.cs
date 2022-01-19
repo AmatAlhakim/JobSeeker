4fc5a17bf663525abf4e485415376a2080d36a47
AmatAlhakim <amatalhakimjammai@gmail.com>
2022-01-19 10:41:54 +0300

    MVC Files Are Added. Commit

--- JobSeeker/Global.asax.cs
+++ JobSeeker/Global.asax.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using System.Threading;
+using System.Globalization;
 
 namespace JobSeeker
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void ApplicationAquirRequestState(object sender, EventArgs e)
+        {
+            string culture = "ar-sy";
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(culture);
         }
     }
 }
